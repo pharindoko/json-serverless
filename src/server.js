60cdc10561b8a9b8aa4885c4383a2fa58a5df5da
flo <you@example.com>
2019-01-13 17:56:41 +0100

    added file for default value

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 'use strict';
-
+const fs = require('fs')
 const low = require('lowdb');
 const AwsAdapter = require('lowdb-adapter-aws-s3')
 const serverless = require('serverless-http');
 const jsonServer = require('json-server');
+const defaultDB = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'));
 const logger = require('pino')({
   prettyPrint: true
 }, process.stderr);
@@ -18,7 +19,7 @@ logger.info('S3FILE: ' + process.env.S3FILE);
 logger.info('S3BUCKET: ' + process.env.S3BUCKET);
 logger.info('READONLY: ' + process.env.READONLY);
 const server = jsonServer.create();
-const storage = new AwsAdapter(process.env.S3FILE, { defaultValue: { "basic": { "hello": "world" } }, aws: { bucketName: process.env.S3BUCKET } });
+const storage = new AwsAdapter(process.env.S3FILE, { defaultValue: defaultDB, aws: { bucketName: process.env.S3BUCKET } });
 
 const request = async () => {
   try {
