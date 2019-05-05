4d4086e029f5ae4add3e5f874c1aa32dec1cf97d
flo <you@example.com>
2019-05-05 20:52:16 +0200

    Fix: fixed server.js - removed unneeded function to reload

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-
-
 const fs = require('fs');
 const low = require('lowdb');
 const AwsAdapter = require('lowdb-adapter-aws-s3');
 const serverless = require('serverless-http');
 const jsonServer = require('json-server');
-const dotenv = require('dotenv');
-const dynamicMiddleware = require('express-dynamic-middleware');
 
 const defaultDB = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'));
 const logger = require('pino')({
@@ -16,7 +12,7 @@ const logger = require('pino')({
 if (process.env.NODE_ENV === 'local') {
   logger.info('load variables from .env file');
   // eslint-disable-next-line global-require
-  dotenv.config();
+  require('dotenv').load();
 }
 
 logger.info(`S3FILE: ${process.env.S3FILE}`);
@@ -35,40 +31,10 @@ const request = async () => {
   try {
     const adapter = await low(storage);
     logger.info('storage initialized');
-    let router = jsonServer.router(adapter);
-    let dynamicRouter = dynamicMiddleware.create(router);
+    const router = jsonServer.router(adapter);
     const middlewares = jsonServer.defaults({ readOnly: process.env.READONLY === 'true' });
-
     server.use(middlewares);
-
-    server.use(jsonServer.bodyParser);
-    // eslint-disable-next-line consistent-return
-    server.use(async (req, res, next) => {
-      logger.info('reload/');
-      if (req.method === 'POST' && req.path === '/reload') {
-        if (req.body && Object.keys(req.body).length) {
-          logger.info(`reload: req.body: ${JSON.stringify(req.body)}`);
-          router.db.setState(req.body);
-          await router.db.write();
-          logger.info('reload: written to database');
-        } else {
-          logger.info('reload: reload database');
-          let state = await router.db.read();
-          if (!state && Object.keys(state).length) {
-            state = defaultDB;
-          }
-          logger.info(`reload: state: ${JSON.stringify(state)}`);
-          router.db = state;
-        }
-        dynamicRouter.unuse(router);
-        router = jsonServer.router(router.db);
-        dynamicRouter = dynamicMiddleware.create(router);
-        server.use(dynamicRouter.handle());
-        return res.sendStatus(200);
-      }
-      next();
-    });
-    server.use(dynamicRouter.handle());
+    server.use(router);
   } catch (e) {
     if (e.code === 'ExpiredToken') {
       logger.error(`Please add valid credentials for AWS. Error: ${e.message}`);
