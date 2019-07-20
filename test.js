a6c7199b94fe2d36b0b19faa1bc1cbcf13adaf72
flo <you@example.com>
2019-07-20 17:45:54 +0200

    Feat: added simple api test with jest

---
+++ test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('./src/utils.js');
+
+describe('Test the root path', () => {
+  test('It should response the GET method', async () => {
+    const response = await request(app.server).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe('Test the root path', () => {
+  test('It should return a default object', async () => {
+    const response = await request(app.server).get('/basic');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.hello).toBe('world');
+  });
+});
