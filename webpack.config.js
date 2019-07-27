3d41ca597b4da08e2589e747d23cedb2d5149cd4
flo <you@example.com>
2019-07-27 15:44:14 +0200

    Refactor: added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new CopyPlugin([
+      { from: './db.json', to: './db.json' },
+    ]),
+  ],
+  entry: slsw.lib.entries,
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
