165e84a310d97abbbf28d37d79a1574f6d1da547
uid10804 <florian.fuss@continental.com>
2019-12-14 20:23:55 +0100

    style(gts): gts fix

--- src/server/utils/logger.ts
+++ src/server/utils/logger.ts
@@ -2,25 +2,26 @@ import * as pino from 'pino';
 
 export class Logger {
   private static instance: pino.Logger;
-  private constructor() { }
+  private constructor() {}
 
-  public static init(prettyPrint = false){
-    Logger.instance = pino.default({
-      prettyPrint: prettyPrint
-    },
-    process.stderr
-  );
- }
+  static init(prettyPrint = false) {
+    Logger.instance = pino.default(
+      {
+        prettyPrint,
+      },
+      process.stderr
+    );
+  }
 
-  static getInstance():pino.Logger {
-      if (!Logger.instance) {
-        return Logger.instance = pino.default({
-          prettyPrint: false
+  static getInstance(): pino.Logger {
+    if (!Logger.instance) {
+      return (Logger.instance = pino.default(
+        {
+          prettyPrint: false,
         },
         process.stderr
-      );
-      }
-      return Logger.instance;
-      
+      ));
+    }
+    return Logger.instance;
   }
 }
