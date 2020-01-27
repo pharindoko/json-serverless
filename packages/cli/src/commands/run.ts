a16c5ee1031b255e07b3367055efcde9562ce47d
uid10804 <florian.fuss@continental.com>
2020-01-27 23:02:06 +0100

    feat(cli): add readonly mode for local run option

--- packages/cli/src/commands/run.ts
+++ packages/cli/src/commands/run.ts
@@ -1,43 +1,55 @@
-import {Command, flags} from '@oclif/command'
-import {startServer, AppConfig} from 'json-serverless-lib'
+import { Command, flags } from '@oclif/command';
+import { startServer, AppConfig } from 'json-serverless-lib';
 import express from 'express';
 import { Helpers } from '../actions/helpers';
 
-
 export class Run extends Command {
-  static description = 'describe the command here'
+  static description = 'describe the command here';
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     // flag with no value (-e, --env)
     env: flags.string({
-    char: 'e',
-    description: 'environment', // help description for flag
-    hidden: false,                // hide from help
-    options: ['development', 'local'],
-    default: 'local',
-    required: false,
-  })
-  }
+      char: 'e',
+      description: 'environment', // help description for flag
+      hidden: false, // hide from help
+      options: ['development', 'local'],
+      default: 'local',
+      required: false,
+    }),
+    readonly: flags.boolean({
+      char: 'r', // shorter flag version
+      description: 'set api to readonly (true) or writeable (false)', // help description for flag
+      hidden: false, // hide from help
+      default: false, // default value if flag not passed (can be a function that returns a string or undefined)
+      required: false, // default value if flag not passed (can be a function that returns a string or undefined)
+    }),
+  };
 
   static args = [
     {
-      name: 'file',               // name of arg to show in help and reference with args[name]
-      required: true,            // make the arg required with `required: true`
+      name: 'file', // name of arg to show in help and reference with args[name]
+      required: true, // make the arg required with `required: true`
       description: 'path of JSON file', // help description
-      hidden: false,               // hide this arg from help
-    }
-  ]
+      hidden: false, // hide this arg from help
+    },
+  ];
 
   async run() {
     await Helpers.generateLogo('json-serverless');
     this.log();
-    const {args, flags} = this.parse(Run)
+    const { args, flags } = this.parse(Run);
     const server = express();
     const defaultConfig = new AppConfig();
+    defaultConfig.readOnly = flags.readonly;
     defaultConfig.jsonFile = args.file;
     if (args.file && flags.env) {
-      startServer(flags.env, server, defaultConfig, this.config.root + '/package.json');
+      startServer(
+        flags.env,
+        server,
+        defaultConfig,
+        this.config.root + '/package.json'
+      );
     }
   }
 }
