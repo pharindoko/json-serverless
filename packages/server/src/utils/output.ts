bc60d3423fc1299a0244df72246cff3f84ba8461
Fuss Florian (uid10804) <florian.fuss@continental.com>
2020-06-08 08:07:39 +0200

    refactor(server): add new validation report

--- packages/server/src/utils/output.ts
+++ packages/server/src/utils/output.ts
@@ -1,5 +1,6 @@
 import { Logger } from './logger';
-
+import { ValidationResult } from '../validations/validationresult';
+import { table } from 'table';
 export class Output {
   static setWarning(message: string) {
     Logger.getInstance().warning(message);
@@ -9,5 +10,34 @@ export class Output {
   }
   static setInfo(message: string) {
     Logger.getInstance().info(message);
+  }
+  static setDebugInfo(message: string) {
+    Logger.getInstance().debug(message);
+  }
+
+  static printValidationReport(validationResult: ValidationResult) {
+    const flattenedRules = new Array<any>();
+    flattenedRules.push(['ValidationRule', 'Result', 'Message']);
+    for (let ruleEvent of validationResult.ruleEventList) {
+      for (let event of ruleEvent.events) {
+        flattenedRules.push([
+          ruleEvent.validationRule,
+          event.result,
+          event.message,
+        ]);
+      }
+    }
+    const output = table(flattenedRules, {
+      columns: {
+        2: {
+          width: 100,
+          wrapWord: true,
+        },
+      },
+      drawHorizontalLine: (index, size) => {
+        return index === 0 || index === 1 || index === size;
+      },
+    });
+    console.log(output);
   }
 }
--- packages/server/src/validations/json.validator.ts
+++ packages/server/src/validations/json.validator.ts
@@ -3,38 +3,31 @@ import {
   HasObjectKeyRule,
   IsObjectRule,
   ValidationRule,
+  EmptyArrayRule,
 } from './validationrule';
+
 import { RuleResultSeverity } from './ruleevent';
-import { Output } from '../utils/output';
+import { ValidationResult } from './validationresult';
 export class JSONValidator {
-  static validate(json: {}): boolean {
-    let isValid = true;
+  static validate(json: {}, specificationDependent: boolean): ValidationResult {
+    const validationResult = new ValidationResult();
     const rules = new Array<ValidationRule>();
     rules.push(new IsObjectRule(json));
     rules.push(new HasObjectKeyRule(json));
     rules.push(new HasIdAttributeRule(json));
+    if (specificationDependent) {
+      rules.push(new EmptyArrayRule(json));
+    }
 
-    Output.setInfo(
-      'ValidationRule:' +
-        'Result'.padStart(60 - 'ValidationRule'.length) +
-        'Message'.padStart(80)
-    );
     for (const rule of rules) {
       const results = rule.executeValidation();
+      validationResult.ruleEventList.push(results);
       for (const result of results.events) {
-        Output.setInfo(
-          results.validationRule +
-            ':' +
-            result.result
-              .toString()
-              .padStart(60 - results.validationRule!.length) +
-            result.message.padStart(80)
-        );
         if (result.result === RuleResultSeverity.ALERT) {
-          isValid = false;
+          validationResult.isValid = false;
         }
       }
     }
-    return isValid;
+    return validationResult;
   }
 }
