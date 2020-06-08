9b7cce84560e040d98943324919396b7ac477b1d
Fuss Florian (uid10804) <florian.fuss@continental.com>
2020-06-08 09:23:33 +0200

    test(server): adapt validation tests to support new structure

--- packages/server/tests/validations.spec.ts
+++ packages/server/tests/validations.spec.ts
@@ -1,53 +1,59 @@
 import fs from 'fs';
-import {HasIdAttributeRule , HasObjectKeyRule, IsObjectRule} from '../src/validations/validationrule'
-import {JSONValidator} from '../src/validations/json.validator';
+import {
+  HasIdAttributeRule,
+  HasObjectKeyRule,
+  IsObjectRule,
+} from '../src/validations/validationrule';
+import { JSONValidator } from '../src/validations/json.validator';
 import { RuleResultSeverity } from '../src/validations/ruleevent';
 
-let validateJSON = "";
+let validateJSON = '';
 beforeAll(() => {
-    validateJSON = JSON.parse(fs.readFileSync('./tests/resources/validate.json', 'UTF-8'))
+  validateJSON = JSON.parse(
+    fs.readFileSync('./tests/resources/validate.json', 'UTF-8')
+  );
 });
-  test('It should have an id attribute', () => {
-    const rule = new HasIdAttributeRule(validateJSON);
-    expect(rule.validate()[0].result).toBe(RuleResultSeverity.OK);
-  });
+test('It should have an id attribute', () => {
+  const rule = new HasIdAttributeRule(validateJSON);
+  expect(rule.validate()[0].result).toBe(RuleResultSeverity.OK);
+});
 
-  test('It should have no id attribute', () => {
-    const rule = new HasIdAttributeRule(validateJSON);
-    expect(rule.validate()[1].result).toBe(RuleResultSeverity.WARNING);
-  });
+test('It should have no id attribute', () => {
+  const rule = new HasIdAttributeRule(validateJSON);
+  expect(rule.validate()[1].result).toBe(RuleResultSeverity.WARNING);
+});
 
-  test('It should have object keys', () => {
-    const rule = new HasObjectKeyRule(validateJSON);
-    expect(rule.validate()[0].result).toBe(RuleResultSeverity.OK);
-  });
+test('It should have object keys', () => {
+  const rule = new HasObjectKeyRule(validateJSON);
+  expect(rule.validate()[0].result).toBe(RuleResultSeverity.OK);
+});
 
-  test('It should have no object keys', () => {
-    const rule = new HasObjectKeyRule(JSON.parse("{}"));
-    expect(rule.validate()[0].result).toBe(RuleResultSeverity.ALERT);
-  });
+test('It should have no object keys', () => {
+  const rule = new HasObjectKeyRule(JSON.parse('{}'));
+  expect(rule.validate()[0].result).toBe(RuleResultSeverity.ALERT);
+});
 
-  test('It should be an Object', () => {
-    const rule = new IsObjectRule(validateJSON);
-    expect(rule.validate()[0].result).toBe(RuleResultSeverity.OK);
-  });
+test('It should be an Object', () => {
+  const rule = new IsObjectRule(validateJSON);
+  expect(rule.validate()[0].result).toBe(RuleResultSeverity.OK);
+});
 
-  test('It should be no Object', () => {
-    const rule = new IsObjectRule(JSON.parse("[]"));
-    expect(rule.validate()[0].result).toBe(RuleResultSeverity.ALERT);
-  });
+test('It should be no Object', () => {
+  const rule = new IsObjectRule(JSON.parse('[]'));
+  expect(rule.validate()[0].result).toBe(RuleResultSeverity.ALERT);
+});
 
-  test('It should return valid rules', () => {
-    const validator = JSONValidator.validate(validateJSON);
-    expect(validator).toBeTruthy();
-  });
+test('It should return valid rules', () => {
+  const validator = JSONValidator.validate(validateJSON, true);
+  expect(validator.isValid).toBeTruthy();
+});
 
-  test('It should return valid rules', () => {
-    const validator = JSONValidator.validate(JSON.parse("[]"));
-    expect(validator).toBeFalsy();
-  });
+test('It should return valid rules', () => {
+  const validator = JSONValidator.validate(JSON.parse('[]'), true);
+  expect(validator.isValid).toBeFalsy();
+});
 
-  test('It should return valid rules', () => {
-    const validator = JSONValidator.validate(JSON.parse("{}"));
-    expect(validator).toBeFalsy();
-  });
+test('It should return valid rules', () => {
+  const validator = JSONValidator.validate(JSON.parse('{}'), true);
+  expect(validator.isValid).toBeFalsy();
+});
