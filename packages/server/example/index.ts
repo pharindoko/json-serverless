6e1f7dd5e251797d87f02629c08d9d0e8eebf599
Fuss Florian (uid10804) <florian.fuss@continental.com>
2020-06-21 10:40:54 +0200

    refactor(server): update example to make it easy to understand how the library works

--- packages/server/example/index.ts
+++ packages/server/example/index.ts
@@ -1,19 +1,39 @@
-import { ServerFactory, AppConfig } from '../src/index';
 import express from 'express';
+import {
+  AppConfig,
+  Swagger,
+  SwaggerConfig,
+  CloudEnvironment,
+  CoreApp,
+  FileStorageAdapter,
+} from '../src/index';
 
 const server = express();
-const defaultConfig = new AppConfig();
-defaultConfig.readOnly = false;
-defaultConfig.jsonFile = 'db.json';
+const appConfig = new AppConfig();
 
-const start = async () => {
-  // do something
-  await ServerFactory.createServer(
-    'local',
-    server,
-    defaultConfig,
-    './package.json'
+const environment = new CloudEnvironment();
+const swagger = new Swagger(
+  server,
+  new SwaggerConfig(appConfig.readOnly, appConfig.enableApiKeyAuth),
+  environment.basePath,
+  appConfig.apiRoutePath,
+  './package.json'
+);
+
+let core: CoreApp | undefined;
+core = new CoreApp(
+  appConfig,
+  server,
+  new FileStorageAdapter('db.json'),
+  swagger,
+  environment
+);
+
+const init = async () => {
+  await core!.setup();
+  console.log(
+    'JSON Server is running under port 3000. Use http://localhost:3000/ to access it'
   );
+  server.listen(3000);
 };
-
-start();
+const initPromise = init();
