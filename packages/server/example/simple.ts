824619a18bf8073d85ad71c4a4518c3191770109
Fuss Florian (uid10804) <florian.fuss@continental.com>
2020-09-14 21:57:40 +0200

    refactor(server): add simple and advanced example

---
+++ packages/server/example/simple.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import {
+  AppConfig,
+  CoreApp,
+  Environment,
+  FileStorageAdapter,
+  PublicStrategy,
+  Swagger,
+  SwaggerConfig,
+} from '../src/index';
+
+const server = express();
+const appConfig = new AppConfig();
+const environment = new Environment();
+
+const swagger = new Swagger(
+  server,
+  new SwaggerConfig(appConfig.readOnly, appConfig.enableApiKeyAuth),
+  environment.basePath,
+  appConfig.routes.apiRoutePath,
+  './package.json',
+  appConfig.routes.swaggerSpecRoutePath
+);
+
+let core = new CoreApp(
+  appConfig,
+  server,
+  new FileStorageAdapter('db.json'),
+  swagger,
+  environment,
+  new PublicStrategy()
+);
+
+const init = async () => {
+  await core!.setup();
+  console.log(
+    'JSON Server is running under port 3000. Use http://localhost:3000/ to access it'
+  );
+  server.listen(3000);
+};
+init();
